b41c14b07fa7dc8f8aa1a36d6fd8a5025b32151a
Seungukson1 <aiden2031242@concordiashanghai.org>
2024-08-04 17:59:53 +0900

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,19 @@
+function o() {
+    var temp = document.getElementById("x").value
+    var temp2 = document.getElementById("y").value
+    console.log(temp)
+    console.log(temp2)
+}
+
+// function admin() {
+//     9
+// }
+
+
+if (temp = "a", temp2 = "b" ) {
+    console.log ("pass")
+}
+else {
+    console.log ("wrong")
+}
+
